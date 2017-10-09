0ca378436ad7d6f64929a1363a40ba75e8029740
LoganStyles <emmaist23@gmail.com>
2017-10-09 06:19:16 +0100

    fb login

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var express = require('express');
-// var everyauth = require('everyauth');
 var bodyParser = require('body-parser');
 var path = require('path');
 var request = require('request');
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var credentials = require('./config/credentials');
+var user = require('./models/user');
+
 
 var passport = require('passport'), 
     TwitterStrategy = require('passport-twitter').Strategy,
@@ -18,9 +19,8 @@ passport.use(new FacebookStrategy({
     callbackURL: credentials.facebook.callback,
     profileFields:['id','displayName','emails','name','gender']
     }, function(accessToken, refreshToken, profile, done) {
-        console.log('passport before profile')
+        //console.log('passport before profile')
         console.log(profile);
-        console.log('passport after profile')
         var me = new user({
             email:profile.emails[0].value,
             name:profile.displayName
@@ -43,29 +43,35 @@ passport.use(new FacebookStrategy({
   }
 ));
 
-// passport.serializeUser(function(user, done) {
-//     console.log(user);
-//     done(null, user._id);
-// });
+passport.serializeUser(function(user, done) {
+    console.log(user);
+    done(null, user._id);
+});
 
-// passport.deserializeUser(function(id, done) {
-//     user.findById(id, function(err, user) {
-//         done(err, user);
-//     });
-// });
+passport.deserializeUser(function(id, done) {
+    user.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
 
 
 var app = express();
 //all middlewares
-var users = require('./routes/users');
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')({
+    resave:false,
+    saveUninitialized:false,
+    secret:credentials.cookieSecret
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+var members = require('./routes/members');
 
 //Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-// app.use(express.bodyParser())
-// 	.use(express.cookieParser('mega joints'))
-// 	.use(express.session())
-// 	.use(everyauth.middleware(app));
 
 //Set Static Path
 app.use(express.static(path.join(__dirname,'public')));
@@ -83,11 +89,55 @@ app.engine('html', exphbs({
 }));
 app.set('view engine', 'html');
 
-app.use('/users',users);
+var mongoose = require('mongoose');
+var opts = {
+    server: {
+        socketOptions: { keepAlive: 1 }
+    }
+};
 
-app.get('/', function(req, res){
-	res.render('index', {});
+// switch(app.get('env')) {
+//     case 'development':
+//         mongoose.connect(credentials.mongo.development.connectionString, opts);
+//         break;
+//     case 'production':
+//         mongoose.connect(credentials.mongo.production.connectionString, opts);
+//         break;
+//     default:
+//         throw new error('Unknown execution environment: ', app.get('env'));
+// }
+
+function isLoggedIn(req, res, next) {
+    req.loggedIn = !!req.user;
+    next();
+}
+
+app.get('/', isLoggedIn, function(req, res) {
+    res.render('index', {
+        loggedIn:req.loggedIn
+    });
 });
+
+app.get('/auth/facebook', passport.authenticate('facebook', {scope:"email"}));
+app.get('/auth/facebook/callback', passport.authenticate('facebook', 
+{ successRedirect: '/', failureRedirect: '/login' }));
+
+app.get('/login', isLoggedIn, function(req, res) {
+    if(req.loggedIn) res.redirect('/');
+    console.log(req.loggedIn);
+    res.render('login', {
+        title:'Login/Registration'
+    });
+});
+
+// 500 error handler (middleware)
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('error');
+});
+
+app.use('/members',members);
 
 app.get('*', function(req, res){
 	res.render('404', {});
