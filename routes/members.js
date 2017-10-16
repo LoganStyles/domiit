5dd52436ebf07b8fe4b101395dbab3db42acd21b
LoganStyles <emmaist23@gmail.com>
2017-10-16 19:15:58 +0100

    modification

--- routes/members.js
+++ routes/members.js
@@ -3,36 +3,36 @@ var router = express.Router();
 var config = require('../config/database');
 var user = require('../models/user');
 
-//Register
-router.post('/register',function(req,res,next){
+// //Register
+// router.post('/register',function(req,res,next){
 
-	let newUser=new user();
-	newUser.username=req.body.username;
-	newUser.password=req.body.password;
-	newUser.displayPic="";
-	newUser.phone="";
-	newUser.displayName="";
-	newUser.email=req.body.email;
+// 	let newUser=new user();
+// 	newUser.username=req.body.username;
+// 	newUser.password=req.body.password;
+// 	newUser.displayPic="";
+// 	newUser.phone="";
+// 	newUser.displayName="";
+// 	newUser.email=req.body.email;
 	
 	
 
-	user.findOne({email:req.body.email}, function(err, u) {
-		var msg;
-            if(!u) {//user does not previously exist
-            	console.log('reg !u findOne in save')
-            	newUser.save(function(err, newUser) {
-            		if(err){res.json({success: false,msg:"registration failed"});
-            	}else{
-            		res.json({success:true,msg:"User registered"});
-            	}
-            });
-            } else {//user previously exists
-            	msg="User already exists";
-            	res.json({success:false,msg:msg});
-            }
-        });
+// 	user.findOne({email:req.body.email}, function(err, u) {
+// 		var msg;
+//             if(!u) {//user does not previously exist
+//             	console.log('reg !u findOne in save')
+//             	newUser.save(function(err, newUser) {
+//             		if(err){res.json({success: false,msg:"registration failed"});
+//             	}else{
+//             		res.json({success:true,msg:"User registered"});
+//             	}
+//             });
+//             } else {//user previously exists
+//             	msg="User already exists";
+//             	res.json({success:false,msg:msg});
+//             }
+//         });
 	
-});
+// });
 
 //Login
 router.post('/login',function(req,res,next){
