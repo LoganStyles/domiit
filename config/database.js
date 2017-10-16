0be434dcd9527cde7854368c2c11e77ebdd957de
LoganStyles <emmaist23@gmail.com>
2017-10-16 13:50:43 +0100

    database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
-var uri = process.env.MONGODB_URI;
+// var uri = process.env.MONGODB_URI;
+var uri =mongodb://domiit:domiit@ds013475.mlab.com:13475/heroku_sdf7bh9m;
 
 mongoose.Promise = global.Promise
 mongoose.connect(uri);
