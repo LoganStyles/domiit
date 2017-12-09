68fee7ad867b43bf62d6bf29ffe0f7dd000bf4ea
LoganStyles <emmaist23@gmail.com>
2017-12-09 12:11:22 +0100

    post books and profile bg

--- config/credentials.js
+++ config/credentials.js
@@ -4,20 +4,20 @@ module.exports = {
     facebook:{
         app_id:"2047896521895761",
         app_secret:"f249877c543a7a3021bc60a2b45b162e",
-        callback:"http://localhost:8000/members/auth/facebook/callback"        
-        // callback:"https://ancient-falls-19080.herokuapp.com/members/auth/facebook/callback"        
+        // callback:"http://localhost:8000/members/auth/facebook/callback"        
+        callback:"https://ancient-falls-19080.herokuapp.com/members/auth/facebook/callback"        
     },
     google:{
         clientID:"7303366212-o9bt7v8r9j8aflrbk7925r4b1o48fgq4.apps.googleusercontent.com",
         clientSecret:"jIUAEuHXQcUp0F1iE3AT9CpY",
-        callback:"http://localhost:8000/members/auth/google/callback"        
-        // callback:"https://ancient-falls-19080.herokuapp.com/members/auth/facebook/callback"        
+        // callback:"http://localhost:8000/members/auth/google/callback"        
+        callback:"https://ancient-falls-19080.herokuapp.com/members/auth/facebook/callback"        
     },
     linkedin:{
         clientID:"78y5uh3ph35mrb",
         clientSecret:"x2VZpil9ZuJV1vDE",
-        callback:"http://localhost:8000/members/auth/linkedin/callback"        
-        // callback:"https://ancient-falls-19080.herokuapp.com/members/auth/linkedin/callback"        
+        // callback:"http://localhost:8000/members/auth/linkedin/callback"        
+        callback:"https://ancient-falls-19080.herokuapp.com/members/auth/linkedin/callback"        
     },
     twitter:{
         consumer_key:"akeyishere",
